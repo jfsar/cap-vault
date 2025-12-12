8d9ada86c6482a37420e29ce370e0d62f50d0e0b
John <john.sarmiento452@gmail.com>
2025-12-12 20:56:04 +0800

    user order listing page

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,8 +1,58 @@
+import type { Metadata } from "next";
+import { formatPrice, formatDateTime, formatId } from "@/lib/utils";
+import { getMyOrders } from "@/db/actions/order.action";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
 
+export const metadata: Metadata = {
+    title: 'My Orders'
+}
 
-function UserOrdersPage() {
+async function UserOrdersPage({ searchParams }: {
+    searchParams: Promise<{
+        page: string;
+    }>
+}) {
+    
+  const { page } = await searchParams;
+  
+  const orders = await getMyOrders({
+    page: Number(page) || 1,
+  });
   return (
-    <div>UserOrdersPage</div>
+    <div className="space-y-2">
+          <h2 className="h2-bold">Orders</h2>
+          <div className="overflow-x-auto">
+              <Table>
+                  <TableHeader>
+                      <TableRow>
+                          <TableHead>ID</TableHead>
+                          <TableHead>DATE</TableHead>
+                          <TableHead>TOTAL</TableHead>
+                          <TableHead>PAID</TableHead>
+                          <TableHead>DELIVERED</TableHead>
+                          <TableHead>ACTIONS</TableHead>
+                      </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                      {orders.data.map((order) => (
+                          <TableRow key={order.id}>
+                              <TableCell>{ formatId(order.id)}</TableCell>
+                              <TableCell>{ formatDateTime(order.createdAt!).dateTime}</TableCell>
+                              <TableCell>{ formatPrice(Number(order.totalPrice))}</TableCell>
+                              <TableCell>{ order.isPaid && order.paidAt ? formatDateTime(order.paidAt!).dateTime : 'Not Paid'}</TableCell>
+                              <TableCell>{order.isDelivered && order.deliveredAt ? formatDateTime(order.deliveredAt!).dateTime : 'Not Delivered'}</TableCell>
+                              <TableCell>
+                                  <Link href={`/order/${order.id}`}>
+                                      <span className="px-2">Details</span>
+                                  </Link>
+                              </TableCell>
+                          </TableRow>
+                      ))}
+                  </TableBody>
+              </Table>
+          </div>
+    </div>
   )
 }
 
